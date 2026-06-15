c8300366ccec3f62283f48dcf1c6118231b5dcc3
Rohan Mia <rohanmia.org@gmail.com>
2026-06-16 03:46:53 +0600

    company profile page created

--- src/components/dashboards/DashboardSlidebar.jsx
+++ src/components/dashboards/DashboardSlidebar.jsx
@@ -1,26 +1,31 @@
 
 import {LayoutSideContent, Bell, Envelope, Gear, House, Magnifier, Person} from "@gravity-ui/icons";
 import {Button, Drawer} from "@heroui/react";
+import Link from "next/link";
+import { CgOrganisation } from "react-icons/cg";
+import { FaBriefcase } from "react-icons/fa";
+import { IoAddCircleOutline } from "react-icons/io5";
 
 export function DashboardSlideBar() {
   const navItems= [
-    {icon: House, label: "Home"},
-    {icon: Magnifier, label: "Search"},
-    {icon: Bell, label: "Notifications"},
-    {icon: Envelope, label: "Messages"},
-    {icon: Person, label: "Profile"},
-    {icon: Gear, label: "Settings"},
+    {icon: House,href:"/dashboards/recruiter", label: "Home"},
+    {icon: FaBriefcase ,href:"/dashboards/recruiter/jobs", label: "Jobs"},
+    {icon: IoAddCircleOutline ,href:"/dashboards/recruiter/jobs/new", label: "Post a Job"},
+    {icon: CgOrganisation  ,href:"/dashboards/recruiter/company", label: "Company Profile"},
+    {icon: Envelope,href:"/recruiter", label: "Messages"},
+    {icon: Person,href:"/recruiter", label: "Profile"},
+    {icon: Gear,href:"/recruiter", label: "Settings"},
     ];
     const navLinks =               <nav className="flex flex-col gap-1">
                 {navItems.map((item) => (
-                  <button
+                  <Link href={item.href}
                     key={item.label}
                     className="flex items-center gap-3 rounded-xl px-3 py-2.5 text-sm text-foreground transition-colors hover:bg-default"
                     type="button"
                   >
                     <item.icon className="size-5 text-muted" />
                     {item.label}
-                  </button>
+                  </Link>
                 ))}
               </nav>
 
